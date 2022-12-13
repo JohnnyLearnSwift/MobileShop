db9771b325209688bab241beb10eb384eb773e4d
JohnyLearnSwift <eugenietyan@gmail.com>
2022-12-13 23:33:39 +0300

    Initial Commit

---
+++ MobileShop/MobileShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileShopApp.swift
+//  MobileShop
+//
+//  Created by Eugenie Tyan on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MobileShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
